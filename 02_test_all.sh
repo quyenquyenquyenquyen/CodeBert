6b3bdc81bf0a81d767eef64f3cdcb5a820f8add6
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-05-09 09:54:26 +0700

    Update 02_test_all.sh

--- 02_test_all.sh
+++ 02_test_all.sh
@@ -16,7 +16,7 @@ CUDA_VISIBLE_DEVICES=0 python run2_12.py --do_test --model_type roberta      \
      	--model_name_or_path microsoft/codebert-base   \
  	    --test_filename /kaggle/input/daataa5/task2_data/t5_data/test.json       \
      	--dev_filename /kaggle/input/daataa5/task2_data/t5_data/val.json      \
-     	--load_model_path /kaggle/input/d/adilyn1/checkpoint-best-bleu/pytorch_model.bin \
+     	--load_model_path /kaggle/input/checkpoint-best-bleu/pytorch_model.bin \
      	--output_dir model/code2review_t5_data_task2/out_beam5_hyp5/   \
  	    --max_source_length 512 --max_target_length 100  --beam_size 5   --train_batch_size 16  --eval_batch_size 16       \
       --learning_rate 5e-5   --train_steps 50000  --eval_steps 5000
