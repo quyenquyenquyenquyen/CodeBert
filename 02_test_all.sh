9b637f733dfc2496b537a98f09ca6222c4798f2c
quyenquyenquyenquyen <nmq1123@gmail.com>
2025-05-19 00:01:46 +0700

    Update 02_test_all.sh

--- 02_test_all.sh
+++ 02_test_all.sh
@@ -1,9 +1,9 @@
 # ## ---Trans Data ---
-# mkdir -p model/code2review_trans_data_task2/out_beam5_hyp5/
+# mkdir model/code2review_trans_data_task2/out_beam5_hyp5/
 # CUDA_VISIBLE_DEVICES=0 python run2_12.py --do_test --model_type roberta      \
 #      	--model_name_or_path microsoft/codebert-base   \
-#  	    --test_filename "/kaggle/input/daataa5/task2_data/trans_data/test.json"       \
-#      	--dev_filename "/kaggle/input/daataa5/task2_data/trans_data/val.json"      \
+#  	    --test_filename ../../DATA/task2_data/trans_data/test.json       \
+#      	--dev_filename ../../DATA/task2_data/trans_data/val.json      \
 #      	--load_model_path model/code2review_trans_data_task2/checkpoint-best-bleu/pytorch_model.bin \
 #      	--output_dir model/code2review_trans_data_task2/out_beam5_hyp5/    \
 #  	    --max_source_length 512 --max_target_length 100  --beam_size 5   --train_batch_size 16  --eval_batch_size 16        \
@@ -11,11 +11,11 @@
 
 
 # ----T5 Data ----
-mkdir -p model/code2review_t5_data_task2/out_beam5_hyp5/
+mkdir model/code2review_t5_data_task2/out_beam5_hyp5/
 CUDA_VISIBLE_DEVICES=0 python run2_12.py --do_test --model_type roberta      \
      	--model_name_or_path microsoft/codebert-base   \
- 	    --test_filename /kaggle/input/daataa5/task2_data/t5_data/test.json       \
-     	--dev_filename /kaggle/input/daataa5/task2_data/t5_data/val.json      \
+ 	    --test_filename /kaggle/input/daataa2-5/task2_data/t5_data/test.json       \
+     	--dev_filename /kaggle/input/daataa2-5/task2_data/t5_data/val.json      \
      	--load_model_path /kaggle/input/checkpoint-best-bleu/pytorch_model.bin \
      	--output_dir model/code2review_t5_data_task2/out_beam5_hyp5/   \
  	    --max_source_length 512 --max_target_length 100  --beam_size 5   --train_batch_size 16  --eval_batch_size 16       \
